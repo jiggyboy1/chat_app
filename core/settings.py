fac3aec40fb3d2acd7c8eb18de9c41c50d5a7ad4
kelechi chukwu <leochukwu48@gmail.com>
2024-03-13 11:27:39 +0100

    initial commit

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dydm#+dkd$^i(w(+e*2ep$notjm!xai%s3xv^-bum8v-1x9t=x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
